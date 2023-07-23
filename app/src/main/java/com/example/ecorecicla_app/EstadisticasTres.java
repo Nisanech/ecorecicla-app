35dc10fa83049c5b23faea85df59854457d7d0aa
Andersson David Sanabria Rojas <andersson.sanabria@gse.com.co>
2023-07-22 23:39:00 -0500

    Ajutes en estadisticas tres, se dejan las opciones del menu con el metodo onclick

--- app/src/main/java/com/example/ecorecicla_app/EstadisticasTres.java
+++ app/src/main/java/com/example/ecorecicla_app/EstadisticasTres.java
@@ -1,8 +1,11 @@
 package com.example.ecorecicla_app;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class EstadisticasTres extends AppCompatActivity {
 
@@ -10,5 +13,23 @@ public class EstadisticasTres extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_estadisticas_tres);
+    }
+
+    public void onHistorialClick(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        Toast.makeText(this, "Opción de Historial seleccionada", Toast.LENGTH_SHORT).show();
+    }
+
+    public void onPorcentajesClick(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        Toast.makeText(this, "Opción de Porcentajes seleccionada", Toast.LENGTH_SHORT).show();
+    }
+
+    public void onGananciasClick(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        Toast.makeText(this, "Opción de Ganancias seleccionada", Toast.LENGTH_SHORT).show();
     }
 }
--- app/src/main/java/com/example/ecorecicla_app/MainActivity.java
+++ app/src/main/java/com/example/ecorecicla_app/MainActivity.java
@@ -1,8 +1,8 @@
 package com.example.ecorecicla_app;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
