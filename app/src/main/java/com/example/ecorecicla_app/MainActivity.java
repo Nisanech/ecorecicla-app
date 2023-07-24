1ad010ddce62977531a9d8ad94de396b716b7002
Semreh88 <semreh88@gmail.com>
2023-07-23 19:44:20 -0500

    Cargue de las vista Pantalla Principal

--- app/src/main/java/com/example/ecorecicla_app/MainActivity.java
+++ app/src/main/java/com/example/ecorecicla_app/MainActivity.java
@@ -2,13 +2,28 @@ package com.example.ecorecicla_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+  Button btn_layout;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+
+    btn_layout = findViewById(R.id.irLayout);
+    btn_layout.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intentIr = new Intent(MainActivity.this, PantallaPrincipal.class);
+                startActivity(intentIr);
+            }
+    });
+
   }
 }
---
+++ app/src/main/java/com/example/ecorecicla_app/PantallaPrincipal.java
@@ -0,0 +1,54 @@
+package com.example.ecorecicla_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class PantallaPrincipal extends AppCompatActivity {
+
+
+    Button btnCategoria;
+    Button btnEstadistica;
+    Button btnConsejo;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pantalla_principal);
+
+        btnCategoria = findViewById(R.id.btnCategorias);
+        btnEstadistica = findViewById(R.id.btnEstadisticas);
+        btnConsejo = findViewById(R.id.btnConsejos);
+
+        btnCategoria.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentCategoria = new Intent(PantallaPrincipal.this, MainActivity.class);
+                startActivity(intentCategoria);
+            }
+        });
+
+
+        btnEstadistica.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentEstadistic = new Intent(PantallaPrincipal.this, MainActivity.class);
+                startActivity(intentEstadistic);
+            }
+
+        });
+
+        btnConsejo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentConsejo = new Intent(PantallaPrincipal.this, MainActivity.class);
+                startActivity(intentConsejo);
+            }
+        });
+
+    }
+}
